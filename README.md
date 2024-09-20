9a2582709c8c611f8ffa1e9896f8bd80d2c68350
Stephen Gutekanst <stephen@hexops.com>
2024-09-20 06:38:52 -0700

    wip
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 * NodeJS v22.9.0 or later
 * `npm install -g pnpm esbuild`
+
+To develop the extension you need to install [this extension](https://marketplace.visualstudio.com/items?itemName=connor4312.esbuild-problem-matchers) which you can do by downloading the extension vsix from that page, then installing it in Codium:
+
+```
+codium --install-extension ./connor4312.esbuild-problem-matchers-0.0.3.vsix
+```
--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,9 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zai" is now active!');
+	console.log('cCongratulations, your extension "zai" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
